097422bf5b99d0b206172fd2d7e8d7964b0dc61d
shrimank <shrimankumbar@gmail.com>
2018-02-02 15:01:30 +0530

    Initial Commit

---
+++ src/models/pages.interface.ts
@@ -0,0 +1,16 @@
+export class Page{
+    
+        productName:string;
+        productTitle:string;
+        productCode:string;
+        logo:string;
+        entity:string;
+        category:string;
+        price:number;
+        fields:any[];
+
+        constructor(){
+            
+        }
+    
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Page }  from '../../models/pages.interface';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  page:Page=new Page();
 
+  constructor(public navCtrl: NavController,private http:HttpClient) {
+    
+  }
+
+  ionViewWillLoad(){
+    console.log("ionViewWillLoad start");
+    
+    this.onLoadPageFields();
+    console.log("ionViewWillLoad  end");
+  }
+
+  async onLoadPageFields(){
+    console.log("loading  page");
+    return await this.http.get('assets/sample/page-fields.json').subscribe((res:Page)=>{
+      console.log("Response:",res);
+      this.page=res;
+      console.log(this.page.productTitle);
+      
+      
+    },(err)=>{
+      console.log("Error:",err);
+      
+    });
+    
+  }
+
+  goBack(){
+    
   }
 
 }
